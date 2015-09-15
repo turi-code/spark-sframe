2114eac1446bef1a8858525f8a68903dcf68fac5
Joseph E. Gonzalez <joseph@dato.com>
2015-09-15 12:57:48 -0700

    temporary fix but updated binary using long-term fix
    Change-Id: Ia0995300cb807fa8d321acd852d1aab0d548e9c4

--- scripts/strip_mac_binary.sh
+++ scripts/strip_mac_binary.sh
@@ -5,26 +5,25 @@ f=src/main/resources/org/graphlab/create/spark_unity_mac
 
 strip -S $f
 
-# Remove data-dev rpath additions:
-rmlist=$(otool -l $f | grep -e "dato-dev/deps")
-
-for p in $rmlist; do
-    echo "Trying to remove $p"
-    install_name_tool -delete_rpath $p
-done
-         
-# make sure that libpython is linked to relative path
-install_name_tool -change libpython2.7.dylib @rpath/libpython2.7.dylib $f || true
-install_name_tool -change /System/Library/Frameworks/Python.framework/Versions/2.7/Python @rpath/libpython2.7.dylib $f || true
-
-# add a reasonable resolution path for mac platform
-install_name_tool -add_rpath /System/Library/Frameworks/Python.framework/Versions/2.7/lib
+install_name_tool -change @rpath/libpython2.7.dylib /System/Library/Frameworks/Python.framework/Versions/2.7/Python $f 
 
 
 
+# # Remove data-dev rpath additions:
+# rmlist=$(otool -l $f | grep -e "dato-dev/deps")
+
+# for p in $rmlist; do
+#     echo "Trying to remove $p"
+#     install_name_tool -delete_rpath $p
+# done
 
 
-# relative=`echo $f | sed 's:\./::g' | sed 's:[^/]*/:../:g' | sed 's:[^/]*$::'`
-# # Now we need ../../ to get to PREFIX/lib
-# rpath="@loader_path/../../$relative"
-# install_name_tool -add_rpath $rpath $f || true
+
+# # make sure that libpython is linked to relative path
+# install_name_tool -change libpython2.7.dylib @rpath/libpython2.7.dylib $f || true
+# install_name_tool -change /System/Library/Frameworks/Python.framework/Versions/2.7/Python @rpath/libpython2.7.dylib $f || true
+
+
+# # add a reasonable resolution path for mac platform
+# install_name_tool -add_rpath /System/Library/Frameworks/Python.framework/Versions/2.7/lib
+
